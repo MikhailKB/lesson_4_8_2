5e742429b498b8f2a9367b10c9ee880337cb19aa
Михаил Козин <Vasago85@mail.ru>
2022-06-05 21:08:53 +0300

    lesson_4_8_2

--- Program.cs
+++ Program.cs
@@ -10,17 +10,18 @@ namespace lesson_4_8_2
             WriteLine("\nВведите длину последовательности и нажмите\"Enter\": ");
             string sequences = ReadLine();
             int n = int.Parse(sequences);
-            int[] numb = new int[n];
+            Random random = new Random();
+            int[] numbs = new int[n];
             WriteLine("\nВведите целые положительные или отрицательные числа и нажмите \"Enter\": ");
             int a = int.Parse(ReadLine()), min = int.MaxValue, val = 0;
-            for (int i = 1; i < numb.Length; i++)
+            for (int i = 1; i < numbs.Length; i++)
             {
+                numbs[i] = random.Next(n);
                 val = Int32.Parse(ReadLine());
                 min = min > val ? val : min;
-            }       
+            }
             WriteLine("min : " + min);
             ReadKey();
-        }
-        
+        }       
     }
 }
